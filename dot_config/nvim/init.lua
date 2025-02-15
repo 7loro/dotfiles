742caf2d5ee3ff3ceb102a3e02974326dc67d938
Casper <nowfeel88@gmail.com>
2025-02-15 09:00:46 +0900

    Update nvim theme config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -138,6 +138,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -564,21 +567,15 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'catppuccin/nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        transparent_background = true, -- 배경 투명하게 설정
+      }
+      vim.cmd.colorscheme 'catppuccin' -- 테마 적용
+      vim.cmd.hi 'Comment gui=none' -- 하이라이트 커스터마이징
     end,
   },
 
