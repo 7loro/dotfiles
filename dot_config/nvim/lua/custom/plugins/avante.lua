95cfc690daaa3bbc4dadfbb09a9f898118d7abfd
Casper <nowfeel88@gmail.com>
2025-03-26 00:49:39 +0900

    set brave for web search in avante

--- dot_config/nvim/lua/custom/plugins/avante.lua
+++ dot_config/nvim/lua/custom/plugins/avante.lua
@@ -14,6 +14,9 @@ return {
       max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
     },
+    web_search_engine = {
+      provider = "brave",
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
