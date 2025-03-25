9d0a7c3cc623465ebc847ac711430ae43ed4d8ee
Casper <nowfeel88@gmail.com>
2025-03-26 00:11:14 +0900

    add avante

--- dot_config/nvim/lua/custom/plugins/harpoon.lua
+++ dot_config/nvim/lua/custom/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set('n', '<leader>a', function()
+    vim.keymap.set('n', '<leader>A', function()
       harpoon:list():add()
     end, { desc = 'Add to harpoon' })
     vim.keymap.set('n', '<C-e>', function()
