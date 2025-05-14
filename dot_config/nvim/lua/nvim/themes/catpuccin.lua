13a8d1161d722e4dcc260272e879973bec8f6857
Casper <nowfeel88@gmail.com>
2025-05-14 14:05:59 +0900

    fix tabline

--- dot_config/nvim/lua/nvim/themes/catpuccin.lua
+++ dot_config/nvim/lua/nvim/themes/catpuccin.lua
@@ -10,6 +10,11 @@ return {
             Visual = { bg = colors.surface2, style = { 'bold' } },
             LineNr = { fg = colors.rosewater }, -- 줄 번호 색상 커스터마이징
             CursorLineNr = { fg = colors.pink, style = { 'bold' } }, -- 현재 줄 번호 색상 커스터마이징
+
+            -- MiniTablineModifiedCurrent에 밑줄 스타일 추가
+            -- fg와 bg를 설정하지 않으면 catppuccin의 기본 색상이 적용됩니다.
+            -- style 테이블에 원하는 스타일 (bold, italic 등)과 함께 'underline'을 추가합니다.
+            MiniTablineModifiedCurrent = { style = { 'bold', 'italic', 'underline' } }, -- 예시: 기존 볼드, 이탤릭에 밑줄 추가
           }
         end
       }
