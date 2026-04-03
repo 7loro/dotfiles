5a42950dd32f3c6529d8d841b0f613fd4f5ef151
Casper <nowfeel88@gmail.com>
2026-04-04 08:42:53 +0900

    update: blink-cmp 빌드 로직 개선 및 mini.statusline 커스텀 설정

--- dot_config/nvim/lua/nvim/plugins/mini.lua
+++ dot_config/nvim/lua/nvim/plugins/mini.lua
@@ -12,7 +12,33 @@ require("mini.pairs").setup()
 require("mini.ai").setup()
 require("mini.indentscope").setup()
 require("mini.cursorword").setup()
-require("mini.statusline").setup()
+require("mini.statusline").setup({
+  content = {
+    active = function()
+      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      local git = MiniStatusline.section_git({ trunc_width = 40 })
+      local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+      local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+      local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+      local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+      local total = vim.fn.line("$")
+      local lw = #tostring(total)
+      local location = string.format("Ln %" .. lw .. "d/%d  Col %-3d", vim.fn.line("."), total, vim.fn.virtcol("."))
+
+      return MiniStatusline.combine_groups({
+        { hl = mode_hl, strings = { mode } },
+        { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
+        "%<",
+        { hl = "MiniStatuslineFilename", strings = { filename } },
+        "%=",
+        { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+        { hl = "MiniStatuslineFileinfo", strings = { search, location } },
+      })
+    end,
+  },
+})
 require("mini.tabline").setup()
 require("mini.comment").setup()
 
